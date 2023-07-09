0f1b9d182930e647709d2e199fe36f7215cd90b9
Roger Tan <teochewthunder@gmail.com>
2023-07-09 15:19:44 +0800

    Create tt_selfaffirmations.php

---
+++ tt_selfaffirmations.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Plugin Name: Self-affirmations
+ * Plugin URI: http://www.teochewthunder.com
+ * Description: This plugin implements self-affirmations
+ * Version: 1.0.0
+ * Author: TeochewThunder
+ * Author URI: http://www.teochewthunder.com
+ * License: GPL2
+ */
+
+function wc_admin_menu() {
+	//this is a test for auth authentication.
+    add_submenu_page( 'index.php', 'Test Readytoreceive', 'Test Readytoreceive', 'manage_options', 'tt_get_readytoreceive', 'tt_get_readytoreceive' );
+	add_submenu_page( 'index.php', 'Test Generate Email', 'Test Generate Email', 'manage_options', 'tt_generate_mail', 'tt_generate_mail' );
+	add_submenu_page( 'index.php', 'Test Job', 'Test Job', 'manage_options', 'tt_selfaffirmations', 'tt_selfaffirmations' );
+}
+
+add_action( 'admin_menu', 'wc_admin_menu', 11 );
+
+function tt_get_readytoreceive() {
+	$cURLConnection = curl_init();
+
+	curl_setopt($cURLConnection, CURLOPT_URL, "https://apex.oracle.com/pls/apex/teochewthunder/mailinglist/readytoreceive");
+	curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+	$json_list = curl_exec($cURLConnection);
+	curl_close($cURLConnection);
+
+	$list = json_decode($json_list);
+	print_r($list->items);
+	
+	return $list->items;
+}
+
+function tt_get_terms($id) {
+	$cURLConnection = curl_init();
+
+	curl_setopt($cURLConnection, CURLOPT_URL, "https://apex.oracle.com/pls/apex/teochewthunder/mailinglist/terms/" . $id);
+	curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+	$json_list = curl_exec($cURLConnection);
+	curl_close($cURLConnection);
+
+	$list = json_decode($json_list);
+	
+	$interests = [];
+	$descriptions = [];
+	foreach($list->items as $i) {
+		if ($i->type == "INTERESTS" && rand(0, 1) == 1) $interests[] = $i->term;
+		if ($i->type == "DESCRIPTIONS" && rand(0, 1) == 1) $descriptions[] = $i->term;
+	}
+	
+	return ["interests" => $interests, "descriptions" => $descriptions];
+}
+
+function tt_set_lastsent($id) {
+	$cURLConnection = curl_init();
+
+	curl_setopt($cURLConnection, CURLOPT_URL, "https://apex.oracle.com/pls/apex/teochewthunder/mailinglist/setreceived/" . $id);
+	curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+	$json_list = curl_exec($cURLConnection);
+	curl_close($cURLConnection);
+}
+
+function tt_generate_mail($id, $name, $gender, $dob) {
+	$terms = tt_get_terms($id);
+	echo print_r($terms);echo "<hr />";
+	$interests = "";
+	if (count($terms["interests"]) > 0) {
+		$interests = ($gender == "M" ? "He" : "She") . " is interested in ";
+		for ($i = 0; $i < sizeof($terms["interests"]); $i++) {
+			$interests .= $terms["interests"][$i];
+			if ($i == sizeof($terms["interests"]) - 1) {
+				$interests .= ". ";
+			} else {
+				$interests .= " and ";
+			}
+		}		
+	}
+
+	$descriptions = "";
+	if (count($terms["descriptions"]) > 0) {
+		$descriptions = ($gender == "M" ? "He" : "She") . " is described as ";		
+		for ($i = 0; $i < sizeof($terms["descriptions"]); $i++) {
+			$descriptions .= $terms["descriptions"][$i];
+			if ($i == sizeof($terms["descriptions"]) - 1) {
+				$descriptions .= ". ";
+			} else {
+				$descriptions .= " and ";
+			}
+		}		
+	}
+	
+	$about = ($gender == "M" ? "man" : "woman");
+	$about .= " named '" . $name . "'";
+	$about .= " born on " . explode("T", $dob)[0];
+
+	$prompt_type = rand(0, 5);
+	$prompt = "";
+	$title = "";
+	
+	switch($prompt_type){
+		case 0: $prompt = "Generate an encouraging three paragraph letter to self"; $title = "Your self-affirmation"; break;
+		case 1: $prompt = "Generate a complimentary poem about"; $title = "A poem for you!"; break;
+		case 2: $prompt = "Generate some positive life advice for"; $title = "Some life advice"; break;
+		case 3: $prompt = "Generate a sample horoscope for"; $title = "Your Zodiac advice"; break;
+		case 4: $prompt = "Generate an encouraging two paragraph letter to self for"; $title = "Your self-affirmation"; break;
+		default: $prompt = "Generate an encouraging one paragraph letter to self for"; $title = "Your self-affirmation"; break;
+	}
+	
+	$final_prompt = $prompt . " " . $about . ". " . $interests . $descriptions;
+	echo $final_prompt;
+	echo "<hr />";
+		
+	return ["title" => $title, "body" => $final_prompt];
+	//api call
+	$key = "xxx";
+	
+    $url = 'https://api.openai.com/v1/chat/completions';  
+    
+    $headers = array(
+        "Authorization: Bearer {$key}",
+        "OpenAI-Organization: org-FUOhDblZb1pxvaY6YylF54gl", 
+        "Content-Type: application/json"
+    );
+    
+    // Define messages
+    $messages = array();
+    $messages["role"] = "user";
+    $messages["content"] = $final_prompt;
+    	
+    // Define data
+    $data = array();
+    $data["model"] = "gpt-3.5-turbo";
+    $data["messages"] = $messages;
+    $data["max_tokens"] = 50;
+
+    // init curl
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    
+    $result = curl_exec($curl);
+    if (curl_errno($curl)) {
+        echo 'Error:' . curl_error($curl);
+    } else {
+        echo print_r($result);
+    }
+    
+    curl_close($curl);	
+}
+
+function tt_selfaffirmations() {
+	$list = tt_get_readytoreceive();
+	print_r($list);
+	foreach($list as $l) {
+		$name = $l->first_name . " " . $l->last_name;
+		$email = tt_generate_mail($l->email, $name, $l->gender, $l->dob);
+		echo print_r($email);
+
+		//if (wp_mail("teochewthunder@gmail.com", $email["title"], $email["body"], "", [] )) {
+			tt_set_lastsent("teochewthunder@gmail.com");
+		//}
+	}
+}
